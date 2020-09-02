d21165b978db4ed067c66de2308c2bcaea71767b
Raaaaaaaay86 <mirror46258@gmail.com>
2020-09-02 12:28:14 +0800

    modify section margin

--- src/views/front/Home.vue
+++ src/views/front/Home.vue
@@ -1,30 +1,14 @@
 <template>
-<div>
+  <div>
     <!--Carousel-->
     <div class="row mt-3">
-      <div
-        id="carouselExampleCaptions"
-        data-ride="carousel"
-        data-interval="5000"
-        class="carousel slide carousel-fade"
-      >
+      <div id="carouselExampleCaptions" data-ride="carousel" data-interval="5000" class="carousel slide carousel-fade">
         <div class="carousel-inner">
           <div class="carousel-item active" style="max-height:500px">
-            <img
-              style
-              src="@/assets/imgs/carousel0102.jpg"
-              class="d-block w-100 position-relative"
-            />
+            <img style src="@/assets/imgs/carousel0102.jpg" class="d-block w-100 position-relative" />
             <!--ROUTER LINK-->
-            <a
-              href="#"
-              class="circleBtn rounded-circle d-none d-md-flex flex-column position-absolute"
-              style="top:40%;left:75%;"
-            >
-              <div
-                class="bg-white rounded-circle d-flex flex-column justify-content-center align-items-center"
-                style="width:150px;height:150px"
-              >
+            <a href="#" class="circleBtn rounded-circle d-none d-md-flex flex-column position-absolute" style="top:40%;left:75%;">
+              <div class="bg-white rounded-circle d-flex flex-column justify-content-center align-items-center" style="width:150px;height:150px">
                 <span class="text-center h6">
                   2020
                   <br />A/W LookBook
@@ -33,47 +17,27 @@
             </a>
           </div>
           <div class="carousel-item" style="max-height:500px">
-            <img
-              style
-              src="@/assets/imgs/carousel0202.jpg"
-              class="d-block w-100 position-relative"
-            />
-            <div
-              class="d-none d-md-block text-white position-absolute"
-              style="top:20%;left:15%"
-            >
-              <p class="mb-0 h2 font-weight-bold">
-                世界上 ,
-              </p>
-              <p class="mb-3 h2 font-weight-bold">
-                只有你最了解自己
-              </p>
+            <img style src="@/assets/imgs/carousel0202.jpg" class="d-block w-100 position-relative" />
+            <div class="d-none d-md-block text-white position-absolute" style="top:20%;left:15%">
+              <p class="mb-0 h2 font-weight-bold">世界上 ,</p>
+              <p class="mb-3 h2 font-weight-bold">只有你最了解自己</p>
               <!--ROUTER LINK-->
               <router-link to="/products/all" class="btn btn-primary">了解更多</router-link>
             </div>
           </div>
         </div>
-        <a
-          class="carousel-control-prev"
-          href="#carouselExampleCaptions"
-          role="button"
-          data-slide="prev"
-        >
+        <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
           <span class="sr-only">Previous</span>
         </a>
-        <a
-          class="carousel-control-next"
-          href="#carouselExampleCaptions"
-          role="button"
-          data-slide="next"
-        >
+        <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
           <span class="carousel-control-next-icon" aria-hidden="true"></span>
           <span class="sr-only">Next</span>
         </a>
       </div>
     </div>
-    <div style="margin-top:60px;margin-bottom:60px;">
+    <!--CAROUSEL & QUOTE-->
+    <div class="my-30 my-md-150">
       <!--QUOTE-->
       <div class="row mb-5">
         <div class="col text-center">
@@ -88,98 +52,78 @@
       <div class="slider">
         <div class="item d-flex justify-content-center" v-for="product in randomProducts" :key="product.id">
           <div class="d-flex flex-column align-items-center justify-content-center" style="width:270px;">
-            <div
-            class="bg-cover"
-            style="width:100%;height:270px"
-            :style="{ backgroundImage: 'url(' + product.imageUrl + ')' }"
-            >
-            </div>
+            <div class="bg-cover" style="width:100%;height:270px" :style="{ backgroundImage: 'url(' + product.imageUrl + ')' }"></div>
           </div>
         </div>
       </div>
     </div>
-  <!--CUBE-->
-    <div class="row no-gutters">
-      <div
-        class="col-md-6 mb-3 mb-md-0 d-flex flex-column justify-content-center align-items-center"
-      >
-        <p class="h2">CLAIRE's代表的是...</p>
-        <p>一種歷久彌新的獨特風格。</p>
-        <router-link
-          to="/products/all"
-          tag="button"
-          class="btn btn-sm btn-outline-primary"
-        >了解更多</router-link>
+    <!--CUBE-->
+    <div class="mb-30 mb-md-150">
+      <div class="row no-gutters">
+        <div class="col-md-6 mb-3 mb-md-0">
+          <div class="bg-cover head01" style="height:280px"></div>
+        </div>
+        <div class="col-md-6 mb-3 mb-md-0 d-flex flex-column justify-content-center align-items-center">
+          <p class="h2">CLAIRE's代表的是...</p>
+          <p>一種歷久彌新的獨特風格。</p>
+          <router-link to="/products/all" tag="button" class="btn btn-sm btn-outline-primary">了解更多</router-link>
+        </div>
       </div>
-      <div class="col-md-6 mb-3 mb-md-0">
-        <div class="bg-cover head01" style="height:300px"></div>
-      </div>
-    </div>
-    <div class="row no-gutters d-flex flex-row-reverse">
-      <div
-        class="col-md-6 mb-3 mb-md-0 d-flex flex-column justify-content-center align-items-center"
-      >
-        <p class="h2">CLAIRE's 的精神宗旨</p>
-        <p>追求美麗的權利 人人公平</p>
-        <router-link to="/about-claires" tag="button" class="btn btn-sm btn-outline-primary">點我閱讀</router-link>
-      </div>
-      <div class="col-md-6">
-        <div class="bg-cover head02" style="height:300px"></div>
+      <div class="row no-gutters d-flex flex-row-reverse">
+        <div class="col-md-6 mb-3 mb-md-0">
+          <div class="bg-cover head02" style="height:280px"></div>
+        </div>
+        <div class="col-md-6 mb-3 mb-md-0 d-flex flex-column justify-content-center align-items-center">
+          <p class="h2">CLAIRE's 的精神宗旨</p>
+          <p>追求美麗的權利 人人公平</p>
+          <router-link to="/about-claires" tag="button" class="btn btn-sm btn-outline-primary">點我閱讀</router-link>
+        </div>
       </div>
     </div>
-  <!--QUOTE-->
-    <div class="row d-flex justify-content-center" style="margin-top:60px;margin-bottom:60px">
-      <div class="col-12 text-center">
+    <!--QUOTE-->
+    <div class="row mb-30 d-flex justify-content-center">
+      <div class="col-10 col-md-6 text-center" style="border-bottom: 3px solid black;">
         <p class="h3">為您量身打造的產品</p>
       </div>
     </div>
-  <!--Recommend-->
+    <!--Recommend-->
     <div class="row no-gutters">
       <div class="col-md-9">
-        <div
-        class="productIntro-img bg-cover"
-        :style="{ backgroundImage: 'url(' + require('@/assets/imgs/make01.jpg') + ')' }"
-        ></div>
+        <div class="productIntro-img bg-cover" :style="{ backgroundImage: 'url(' + require('@/assets/imgs/make01.jpg') + ')' }"></div>
       </div>
       <div class="col-md-3">
         <div class="productIntro my-3">
-          <div class="productIntro-title  mb-1 d-flex flex-column">
+          <div class="productIntro-title mb-1 d-flex flex-column">
             <p>唇彩系列</p>
           </div>
           <div class="productIntro-content text-primary d-flex flex-column">
             <p class="mb-0">質地滑順 接觸雙唇完美融合</p>
             <p class="mb-0">讓人愛不釋手</p>
           </div>
-          <router-link to="/products/lip" tag="button" class="mt-3 btn  btn-outline-primary">發現更多</router-link>
+          <router-link to="/products/lip" tag="button" class="mt-3 btn btn-outline-primary">發現更多</router-link>
         </div>
       </div>
     </div>
-    <div class="row no-gutters d-flex flex-row-reverse mt-15 mt-md-100">
+    <div class="row no-gutters d-flex flex-row-reverse mt-15 mt-md-30">
       <div class="col-md-9">
-        <div
-        class="productIntroReverse-img bg-cover"
-        :style="{ backgroundImage: 'url(' + require('@/assets/imgs/make03.jpg') + ')' }"
-        ></div>
+        <div class="productIntroReverse-img bg-cover" :style="{ backgroundImage: 'url(' + require('@/assets/imgs/make03.jpg') + ')' }"></div>
       </div>
       <div class="col-md-3">
         <div class="productIntroReverse my-3">
-          <div class="productIntroReverse-title  mb-1 d-flex flex-column">
+          <div class="productIntroReverse-title mb-1 d-flex flex-column">
             <p>腮紅系列</p>
           </div>
           <div class="productIntroReverse-content text-primary d-flex flex-column">
             <p class="mb-0">一抹瑕疵秒神隱 遠觀近看都無懈可擊</p>
             <p class="mb-0">重回自信光彩</p>
           </div>
-          <router-link to="/products/blush" tag="button" class="mt-3 btn  btn-outline-primary">發現更多</router-link>
+          <router-link to="/products/blush" tag="button" class="mt-3 btn btn-outline-primary">發現更多</router-link>
         </div>
       </div>
     </div>
-    <div class="row no-gutters mt-15 mt-md-100">
+    <div class="row no-gutters mt-15 mt-md-30">
       <div class="col-md-9">
-        <div
-        class="productIntro-img bg-cover"
-        :style="{ backgroundImage: 'url(' + require('@/assets/imgs/make02.jpg') + ')' }"
-        ></div>
+        <div class="productIntro-img bg-cover" :style="{ backgroundImage: 'url(' + require('@/assets/imgs/make02.jpg') + ')' }"></div>
       </div>
       <div class="col-md-3">
         <div class="productIntro my-3">
@@ -190,11 +134,11 @@
             <p class="mb-0">顏色繽紛飽滿 光亮持久</p>
             <p class="mb-0">展現個人魅力</p>
           </div>
-          <router-link to="/products/nail" tag="button" class="mt-3 btn  btn-outline-primary">發現更多</router-link>
+          <router-link to="/products/nail" tag="button" class="mt-3 btn btn-outline-primary">發現更多</router-link>
         </div>
       </div>
     </div>
-</div>
+  </div>
 </template>
 
 <script>
@@ -211,7 +155,6 @@ export default {
   },
   methods: {
     getRandomProducts() {
-      // this.$store.dispatch('getProducts', { page, cat: 'all', for: 'carousel' });
       this.$store.dispatch('getRandom');
     },
   },
@@ -252,8 +195,9 @@ export default {
             },
           },
           {
-            breakpoint: 480,
+            breakpoint: 576,
             settings: {
+              dots: false,
               slidesToShow: 1,
               slidesToScroll: 1,
             },
@@ -275,105 +219,105 @@ export default {
   }
 }
 
-.productIntro{
+.productIntro {
   position: absolute;
   top: 20%;
   left: -15%;
-  @media(max-width: 768px){
+  @media (max-width: 768px) {
     display: flex;
-    margin-top:10px;
+    margin-top: 10px;
     flex-direction: column;
-    position:relative;
-    top:0px;
-    left:0px;
+    position: relative;
+    top: 0px;
+    left: 0px;
   }
-  &-img{
+  &-img {
     height: 400px;
   }
-  &-title{
+  &-title {
     font-size: 2.5rem;
     background-color: black;
-    color:white;
+    color: white;
     font-weight: 500;
     line-height: 1.2;
     width: 80%;
-    .p{
-    margin-bottom: 0.5rem;
+    .p {
+      margin-bottom: 0.5rem;
     }
-    @media(max-width: 995px){
+    @media (max-width: 995px) {
       font-size: 2rem;
       font-weight: 500;
       line-height: 1.2;
-      width:70%;
-      .p{
-      margin-bottom: 0.5rem;
+      width: 70%;
+      .p {
+        margin-bottom: 0.5rem;
       }
     }
-    @media(max-width: 768px){
+    @media (max-width: 768px) {
       background-color: transparent;
-      color:black;
+      color: black;
       margin-bottom: 0.25rem;
     }
   }
-  &-content{
-    font-size:18px;
-    @media(max-width: 995px){
+  &-content {
+    font-size: 18px;
+    @media (max-width: 995px) {
       font-size: 16px;
     }
   }
 }
 
-.productIntroReverse{
+.productIntroReverse {
   position: absolute;
   top: 20%;
   right: -35%;
-  @media(max-width: 768px){
+  @media (max-width: 768px) {
     display: flex;
-    margin-top:10px;
+    margin-top: 10px;
     flex-direction: column;
-    position:relative;
-    top:0px;
-    left:0px;
+    position: relative;
+    top: 0px;
+    left: 0px;
   }
-  &-img{
+  &-img {
     height: 400px;
   }
-  &-title{
+  &-title {
     font-size: 2.5rem;
     background-color: black;
-    color:white;
+    color: white;
     font-weight: 500;
     line-height: 1.2;
     width: 65%;
-    .p{
-    margin-bottom: 0.5rem;
+    .p {
+      margin-bottom: 0.5rem;
     }
-    @media(max-width: 995px){
+    @media (max-width: 995px) {
       font-size: 2rem;
       font-weight: 500;
       line-height: 1.2;
-      width:60%;
-      .p{
-      margin-bottom: 0.5rem;
+      width: 60%;
+      .p {
+        margin-bottom: 0.5rem;
       }
     }
-    @media(max-width: 768px){
+    @media (max-width: 768px) {
       background-color: transparent;
-      color:black;
+      color: black;
       margin-bottom: 0.25rem;
     }
   }
-  &-content{
-    font-size:18px;
-    @media(max-width: 995px){
+  &-content {
+    font-size: 18px;
+    @media (max-width: 995px) {
       font-size: 16px;
     }
   }
 }
 
-.fornail{
+.fornail {
   width: 100% !important;
-  @media(max-width: 995px){
+  @media (max-width: 995px) {
     width: 85% !important;
   }
 }
@@ -393,5 +337,4 @@ export default {
     box-shadow: 0px 0px 10px black;
   }
 }
-
 </style>
--- src/views/front/Product.vue
+++ src/views/front/Product.vue
@@ -1,45 +1,24 @@
 <template>
   <div>
     <div class="row mt-3 d-flex align-items-center justify-content-center">
-      <div
-        class="col-md-10 pl-5 pt-5 bg-cover product-header"
-        style="height:350px"
-      >
+      <div class="col-md-10 pl-5 pt-5 bg-cover product-header" style="height:350px">
         <p class="h3 text-primary font-weight-bolder">2020秋冬 全新感受</p>
         <small class="d-block mb-3 text-primary">CLAIRE'S團隊打造的新色彩美學</small>
-        <router-link
-          to="/checkout"
-          tag="button"
-          class="btn btn-sm btn-primary"
-        >查看購物車</router-link>
+        <router-link to="/checkout" tag="button" class="btn btn-sm btn-primary">查看購物車</router-link>
       </div>
     </div>
 
     <transition-group tag="div" name="list" class="row mt-3 d-flex justify-content-center no-gutters">
-      <router-link
-        tag="a"
-        :to="{ name: 'detail', params: { id: product.id } }"
-        class="col-6 col-md-3 mb-3 d-flex justify-content-center"
-        v-for="product in products"
-        :key="product.id"
-      >
+      <router-link tag="a" :to="{ name: 'detail', params: { id: product.id } }" class="col-6 col-md-3 mb-3 d-flex justify-content-center" v-for="product in products" :key="product.id">
         <div class="card">
-          <div
-            class="card-img-top bg-contain"
-            style="min-height:150px"
-            :style="{ backgroundImage: `url(${product.imageUrl})` }"
-          ></div>
+          <div class="card-img-top bg-contain" style="min-height:150px" :style="{ backgroundImage: `url(${product.imageUrl})` }"></div>
           <div class="card-body">
             <div class="text-center">
-              <router-link
-                :to="{ name: 'detail', params: { id: product.id } }"
-                tag="a"
-                class="card-title mb-0 font-weight-bolder"
-              >
-                {{ product.title }}
-              </router-link>
+              <router-link :to="{ name: 'detail', params: { id: product.id } }" tag="a" class="card-title mb-0 font-weight-bolder">{{ product.title }}</router-link>
               <p class="card-text mb-1">{{ product.description }}</p>
-              <small><del>原價: {{ product.origin_price | currency }}</del></small>
+              <small>
+                <del>原價: {{ product.origin_price | currency }}</del>
+              </small>
               <p class="mb-3">{{ product.price | currency }}</p>
               <a href="#" class="d-block link-add mt-2 btn btn-lg btn-primary" @click.prevent="addToCart(product.id)">
                 <i class="fas fa-shopping-cart"></i>
@@ -99,7 +78,7 @@ a {
 .card {
   transition: 0.1s;
   width: 75%;
-  @media(max-width: 768px) {
+  @media (max-width: 768px) {
     width: 90%;
   }
   &-text {
@@ -107,7 +86,7 @@ a {
   }
   &:hover {
     transform: scale(1.02);
-    box-shadow: 0px 3px 5px rgba(189,195,199,0.7);
+    box-shadow: 0px 3px 5px rgba(189, 195, 199, 0.7);
   }
   &-body {
     padding: 20px 5px;
@@ -119,5 +98,4 @@ a {
     font-size: 14px;
   }
 }
-
 </style>
